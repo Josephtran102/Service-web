a7541dbd975ec415dcce55e81dd9b95dc58788e0
vladsubbotin1 <vladislav.subbotin2010@gmail.com>
2023-04-10 19:36:45 +0400

    feat: dark theme localstorage

--- context/context.jsx
+++ context/context.jsx
@@ -1,18 +1,36 @@
 import { changeStyles } from 'utils/changeStyles'
-import { useState, createContext } from 'react'
+import { useState, createContext, useEffect } from 'react'
 
 export const Context = createContext()
 
+const setThemeInStorage = theme => {
+	localStorage.setItem('theme', theme)
+}
+const getThemeInStorage = () => {
+	return localStorage.getItem('theme')
+}
+
 export const ContextProvider = props => {
-	const [theme, setTheme] = useState('light')
+	const [theme, setTheme] = useState()
+
+	useEffect(() => {
+		if (getThemeInStorage() === 'dark') {
+			setTheme('dark')
+			changeStyles('light')
+		} else {
+			setTheme('light')
+			changeStyles('dark')
+		}
+	}, [])
 
 	const toggleTheme = () => {
 		if (theme === 'light') {
 			setTheme(theme => (theme = 'dark'))
+			setThemeInStorage('dark')
 		} else {
 			setTheme(theme => (theme = 'light'))
+			setThemeInStorage('light')
 		}
-
 		changeStyles(theme)
 	}
 
