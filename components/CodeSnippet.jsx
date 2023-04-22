cae182b7133ce4baa13a978051083532a823242f
vladsubbotin1 <vladislav.subbotin2010@gmail.com>
2023-04-22 22:04:15 +0400

    fix: cicd

--- components/CodeSnippet.jsx
+++ components/CodeSnippet.jsx
@@ -2,7 +2,7 @@ import {
 	atomOneLight,
 	dracula,
 } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
-import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
+import SyntaxHighlighter from 'react-syntax-highlighter'
 import styles from '@styles/CodeSnippet.module.scss'
 import CopyButton from './CopyButton'
 import { useContext } from 'react'
@@ -11,24 +11,13 @@ import { Context } from '@context/context'
 const CodeSnippet = props => {
 	const { theme, toggleTheme } = useContext(Context)
 
-	const getTheme = () => {
-		switch (theme) {
-			case 'dark':
-				return dracula
-			case 'light':
-				return atomOneLight
-			default:
-				return atomOneLight
-		}
-	}
-
 	return (
 		<div className={styles.code__wrapper}>
 			<CopyButton code={props.code} theme={theme} />
 			<SyntaxHighlighter
 				className={styles.code}
 				language='bash'
-				style={getTheme()}
+				style={theme === 'dark' ? dracula : atomOneLight}
 				showLineNumbers={false}
 			>
 				{props.code}
