d766f0f7cd26db93fc26247a97c8421e4a41daa3
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2023-04-29 22:15:01 +0400

    Update projects.jsx

--- store/projects.jsx
+++ store/projects.jsx
@@ -877,7 +877,7 @@ let projects = {
 			installBin:
 				'cd $HOME\nrm -rf ~/terp-core\ngit clone https://github.com/terpnetwork/terp-core.git\ncd terp-core\ngit checkout v0.4.0\nmake install',
 			updHeight: '1037222',
-			newInstallBin: 'sudo systemctl stop terpd.service
+			newInstallBin: '`sudo systemctl stop terpd.service
 cd $HOME
 rm -rf ~/terp-core
 git clone https://github.com/terpnetwork/terp-core.git
@@ -887,7 +887,7 @@ make install
 curl -s  https://raw.githubusercontent.com/terpnetwork/test-net/master/90u-1/genesis.json > $HOME/.terp/config/genesis.json
 terpd tendermint unsafe-reset-all --home $HOME/.terp --keep-addr-book
 sed -i 's/minimum-gas-prices = "[^"]*"/minimum-gas-prices = "0.0002uthiol"/' $HOME/.terp/config/app.toml 
-sudo systemctl restart terpd && sudo journalctl -u terpd -f',
+sudo systemctl restart terpd && sudo journalctl -u terpd -f'`,
 			goVersion: '1.19.3',
 			gas: '--gas auto --gas-adjustment 1.5',
 			unsafeReset: 'tendermint unsafe-reset-all',
