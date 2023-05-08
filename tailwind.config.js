a26541c6c910b97c262ac877981cbf2e2e566466
vladsubbotin1 <vladislav.subbotin2010@gmail.com>
2023-05-08 20:04:48 +0400

    upd deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-const withMT = require('@material-tailwind/react/utils/withMT')
-
-module.exports = withMT({
+module.exports = {
 	content: [
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}',
@@ -13,4 +11,4 @@ module.exports = withMT({
 		extend: {},
 	},
 	plugins: [],
-})
+}
