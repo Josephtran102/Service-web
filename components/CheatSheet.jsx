3c6d540885979f219406b43bd0b77b9dd05eeeb0
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2023-05-13 15:58:46 +0400

    Update CheatSheet.jsx

--- components/CheatSheet.jsx
+++ components/CheatSheet.jsx
@@ -301,7 +301,6 @@ ${gas} \\
 							'Check Validator key',
 							`[[ $(${bin} q staking validator $VALOPER_ADDRESS -oj | jq -r .consensus_pubkey.key) = $(${bin} status | jq -r .ValidatorInfo.PubKey.value) ]] && echo -e "Your key status is ok" || echo -e "Your key status is error"`
 						)}
-						)}
 						{CodeBlock(
 							'Signing info',
 							`${bin} q slashing signing-info $VALOPER_ADDRESS`
