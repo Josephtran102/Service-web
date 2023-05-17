0ca0a63f594223a53b7e0476f86b41fc5971f91e
vladsubbotin1 <vladislav.subbotin2010@gmail.com>
2023-05-17 20:56:37 +0400

    project styles

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -239,7 +239,7 @@ export default function Dashboard(props) {
 					>
 						<div className={styles.stats}>
 							<div className={styles.chain__wrapper} onClick={showModal}>
-								<span className='flex items-center gap-2'>
+								<span className='flex items-center gap-2 select-none'>
 									<RetweetOutlined />
 									<b className={styles.bold}>
 										{name.charAt(0).toUpperCase() + name.slice(1)}
