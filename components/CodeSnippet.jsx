ac04337dcaacc5e88fb395765f8ee8e35d0b7d7d
vladsubbotin1 <vladislav.subbotin2010@gmail.com>
2023-05-19 09:56:13 +0400

    refactor: code snippet import

--- components/CodeSnippet.jsx
+++ components/CodeSnippet.jsx
@@ -1,11 +1,14 @@
 import { atomOneLight, dracula } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
-import { LightAsync as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
+const bash = require('react-syntax-highlighter/dist/esm/languages/hljs/bash').default
 
 import styles from '@styles/CodeSnippet.module.scss'
 import CopyButton from './CopyButton'
 import { useContext } from 'react'
 import { Context } from '@context/context'
 import { useHorizontalScroll } from 'hooks/useHorizontalScroll'
+
+SyntaxHighlighter.registerLanguage('bash', bash)
 
 const CodeSnippet = props => {
 	const { theme, toggleTheme } = useContext(Context)
