f5a6422f56ab7ac51fca7518dd85486ae3768cd9
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2023-05-23 12:14:54 +0400

    Update Installation.jsx

--- components/Installation.jsx
+++ components/Installation.jsx
@@ -242,7 +242,7 @@ sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
 rm "go$VER.linux-amd64.tar.gz"
 [ ! -f ~/.bash_profile ] && touch ~/.bash_profile
 echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
-source ~/.bash_profile
+source $HOME/.bash_profile
 }
 [ ! -d ~/go/bin ] && mkdir -p ~/go/bin
 
