6c58bd460ce78521702954be20962641488e0f9b
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2023-05-26 00:01:01 +0400

    Update projects.jsx

--- data/projects.jsx
+++ data/projects.jsx
@@ -615,7 +615,7 @@ sudo mv $HOME/humans/build/humansd $(which humansd)`,
 			peerPort: '28656',
 			seedPort: '28656',
 			installBin:
-				'cd $HOME\nwget wget https://github.com/KYVENetwork/kyvejs/releases/download/%40kyvejs%2Ftendermint-bsync%401.0.0-beta.10/kyve-linux-arm64.zip\ntar -xvzf $HOME/kyve-linux-arm64.zip\nchmod +x ./kyved\nmv kyved $HOME/go/bin/\nrm $HOME/kyve-linux-arm64.zip',
+				'cd $HOME\nwget wget https://github.com/KYVENetwork/kyvejs/releases/download/%40kyvejs%2Ftendermint-bsync%401.0.0-beta.10/kyve-linux-arm64.zip\nunzip $HOME/kyve-linux-arm64.zip\nchmod +x ./kyved\nmv kyved $HOME/go/bin/\nrm $HOME/kyve-linux-arm64.zip',
 			updHeight: '',
 			newInstallBin: '',
 			goVersion: '1.19.3',
