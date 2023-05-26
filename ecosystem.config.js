b31d23218f1ed4e8b1d7d8cae3ed45f2fece7950
vladsubbotin1 <vladislav.subbotin2010@gmail.com>
2023-05-27 00:26:54 +0400

    add captcha

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	apps: [
+		{
+			name: 'Your App Name',
+			script: 'npm',
+			args: 'start',
+			env_production: {
+				NEXT_PUBLIC_RECAPTCHA_SITE_KEY: '6LfvT0ImAAAAAP8OGvO7NJPTKR2Pm6EuoESfB2pT',
+				RECAPTCHA_SECRET_KEY: '6LfvT0ImAAAAABsqLyW8gjHN-gaiXiZR9xujdoyt'
+			}
+		}
+	]
+}
--- pages/submit_pfb.jsx
+++ pages/submit_pfb.jsx
@@ -5,163 +5,184 @@ import { getRandomHex } from '@utils/getRandomHex'
 import { submitData } from '@utils/pfb'
 import { Form, Input, Button, notification, Modal } from 'antd'
 import Head from 'next/head'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import ReCAPTCHA from 'react-google-recaptcha'
 
 const { TextArea } = Input
 
 const SubmitPFB = () => {
-   const [api, contextHolder] = notification.useNotification()
-   const [res, setRes] = useState(null)
-   const [txHash, setTxHash] = useState(null)
-   const [form] = Form.useForm()
-   const [loading, setLoading] = useState(false)
-   const [opacity, setOpacity] = useState(0)
-   const [isModalOpen, setIsModalOpen] = useState(false)
+	const [api, contextHolder] = notification.useNotification()
+	const [res, setRes] = useState(null)
+	const [txHash, setTxHash] = useState(null)
+	const [form] = Form.useForm()
+	const [loading, setLoading] = useState(false)
+	const [opacity, setOpacity] = useState(0)
+	const [isModalOpen, setIsModalOpen] = useState(false)
+	const [captchaValue, setCaptchaValue] = useState(null)
 
-   const showModal = () => {
-      setIsModalOpen(true)
-      setLoading(false)
-   }
+	const showModal = () => {
+		setIsModalOpen(true)
+		setLoading(false)
+	}
 
-   const openNotification = () => {
-      api['info']({
-         message: 'Your request was submitted',
-         description: 'It may take a while. Please do not close the page.',
-         duration: 5.5
-      })
-   }
+	const openNotification = () => {
+		api['info']({
+			message: 'Your request was submitted',
+			description: 'It may take a while. Please do not close the page.',
+			duration: 5.5
+		})
+	}
 
-   const handleSubmit = async data => {
-      setLoading(true)
-      openNotification()
-      data = {
-         ...data,
-         gas_limit: 80000,
-         fee: 2000
-      }
-      submitData(data).then(response => {
-         console.log(response)
-         setRes(response)
-         setTxHash(response.txhash)
-         showModal()
-      })
-   }
+	const handleCaptchaResponse = value => {
+		setCaptchaValue(value)
+	}
 
-   const generateFields = () => {
-      form.setFieldsValue({
-         namespace_id: getRandomHex(16),
-         data: getRandomHex(40)
-      })
-   }
+	const handleSubmit = async data => {
+		if (captchaValue === null) {
+			alert('Please submit the captcha')
+			return
+		}
+		setLoading(true)
+		openNotification()
+		data = {
+			...data,
+			gas_limit: 80000,
+			fee: 2000,
+			captcha: captchaValue
+		}
+		submitData(data)
+			.then(response => {
+				console.log(response)
+				setRes(response)
+				setTxHash(response.txhash)
+				showModal()
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	}
 
-   const handleOk = () => {
-      setIsModalOpen(false)
-   }
+	const generateFields = () => {
+		form.setFieldsValue({
+			namespace_id: getRandomHex(16),
+			data: getRandomHex(40)
+		})
+	}
 
-   const ClearFields = () => {
-      form.setFieldsValue({
-         namespace_id: '',
-         data: ''
-      })
-   }
+	const handleOk = () => {
+		setIsModalOpen(false)
+	}
 
-   useEffect(() => {
-      generateFields()
-      setOpacity(1)
-   }, [])
+	const ClearFields = () => {
+		form.setFieldsValue({
+			namespace_id: '',
+			data: ''
+		})
+	}
 
-   return (
-      <div
-         className='w-screen h-screen flex flex-wrap flex-col justify-center items-center'
-         style={{ opacity: opacity, transition: '0.8s' }}
-      >
-         {contextHolder}
-         <Head>
-            <title>Celestia Submit PayForBlob - ITRocket</title>
-            <meta
-               name='description'
-               content='ITRocket 🚀 - Crypto multipurpose project focused on providing best services for Cosmos (and not only) node operators'
-            />
-         </Head>
+	useEffect(() => {
+		generateFields()
+		setOpacity(1)
+	}, [])
 
-         <ParticlesBG />
+	return (
+		<div
+			className='w-screen h-screen flex flex-wrap flex-col justify-center items-center'
+			style={{ opacity: opacity, transition: '0.8s' }}
+		>
+			{contextHolder}
+			<Head>
+				<title>Celestia Submit PayForBlob - ITRocket</title>
+				<meta
+					name='description'
+					content='ITRocket 🚀 - Crypto multipurpose project focused on providing best services for Cosmos (and not only) node operators'
+				/>
+			</Head>
 
-         <Modal
-            title='Your request was successfully submitted! 🎉'
-            open={isModalOpen}
-            onOk={handleOk}
-            cancelButtonProps={{ style: { display: 'none' } }}
-            closable={false}
-            style={{ minWidth: '50%' }}
-         >
-            <div className='flex flex-col gap-4'>
-               <p>
-                  Your Tx Hash is: <span className='font-bold'>{txHash}</span>
-               </p>
-               <p>
-                  <QuestionCircleTwoTone className='align-middle pr-1' />
-                  You can check the transaction status here:{' '}
-                  <a
-                     href={`https://testnet.mintscan.io/celestia-incentivized-testnet/txs/${txHash}`}
-                     style={{ color: '#055bd5' }}
-                     target='_blank'
-                     rel='noopener referrer'
-                  >
-                     https://testnet.mintscan.io/celestia-incentivized-testnet/txs/
-                     {txHash}
-                  </a>
-               </p>
-               <TextArea value={JSON.stringify(res, null, 2)} autoSize={{ minRows: 5, maxRows: 10 }} />
-            </div>
-         </Modal>
+			<ParticlesBG />
 
-         <h1 className='text-3xl font-bold mb-4 tracking-tight'>PayForBlob Transaction</h1>
+			<Modal
+				title='Your request was successfully submitted! 🎉'
+				open={isModalOpen}
+				onOk={handleOk}
+				cancelButtonProps={{ style: { display: 'none' } }}
+				closable={false}
+				style={{ minWidth: '50%' }}
+			>
+				<div className='flex flex-col gap-4'>
+					<p>
+						Your Tx Hash is: <span className='font-bold'>{txHash}</span>
+					</p>
+					<p>
+						<QuestionCircleTwoTone className='align-middle pr-1' />
+						You can check the transaction status here:{' '}
+						<a
+							href={`https://testnet.mintscan.io/celestia-incentivized-testnet/txs/${txHash}`}
+							style={{ color: '#055bd5' }}
+							target='_blank'
+							rel='noopener referrer'
+						>
+							https://testnet.mintscan.io/celestia-incentivized-testnet/txs/
+							{txHash}
+						</a>
+					</p>
+					<TextArea value={JSON.stringify(res, null, 2)} autoSize={{ minRows: 5, maxRows: 10 }} />
+				</div>
+			</Modal>
 
-         <Form
-            form={form}
-            onFinish={handleSubmit}
-            style={{ minWidth: '50%', opacity: loading === true ? '0.7' : '1' }}
-            layout='vertical'
-            className='flex flex-col  flex-wrap bg-white p-12 rounded-xl border-solid border-[1px]  border-slate-200 hover:border-slate-300 transition-all'
-         >
-            <Form.Item
-               name='namespace_id'
-               label='Namespace_id'
-               rules={[
-                  {
-                     required: true
-                  }
-               ]}
-            >
-               <Input maxLength={16} showCount />
-            </Form.Item>
-            <Form.Item
-               name='data'
-               label='Data'
-               rules={[
-                  {
-                     required: true
-                  }
-               ]}
-            >
-               <Input maxLength={200} showCount />
-            </Form.Item>
-            <Form.Item>
-               <div className='flex justify-center gap-2'>
-                  <Button size='large' type='primary' htmlType='submit' loading={loading}>
-                     Submit
-                  </Button>
-                  <Button size='large' onClick={generateFields}>
-                     Generate
-                  </Button>
-                  <Button type='dashed' size='large' onClick={ClearFields}>
-                     Reset
-                  </Button>
-               </div>
-            </Form.Item>
-         </Form>
-      </div>
-   )
+			<h1 className='text-3xl font-bold mb-4 tracking-tight'>PayForBlob Transaction</h1>
+
+			<Form
+				form={form}
+				onFinish={handleSubmit}
+				style={{ minWidth: '50%', opacity: loading === true ? '0.7' : '1' }}
+				layout='vertical'
+				className='flex flex-col  flex-wrap bg-white p-12 rounded-xl border-solid border-[1px]  border-slate-200 hover:border-slate-300 transition-all'
+			>
+				<Form.Item
+					name='namespace_id'
+					label='Namespace_id'
+					rules={[
+						{
+							required: true
+						}
+					]}
+				>
+					<Input maxLength={16} showCount />
+				</Form.Item>
+				<Form.Item
+					name='data'
+					label='Data'
+					rules={[
+						{
+							required: true
+						}
+					]}
+				>
+					<Input maxLength={200} showCount />
+				</Form.Item>
+				<Form.Item>
+					<ReCAPTCHA
+						sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+						onChange={handleCaptchaResponse}
+					/>
+				</Form.Item>
+				<Form.Item>
+					<div className='flex justify-center gap-2'>
+						<Button size='large' type='primary' htmlType='submit' loading={loading}>
+							Submit
+						</Button>
+						<Button size='large' onClick={generateFields}>
+							Generate
+						</Button>
+						<Button type='dashed' size='large' onClick={ClearFields}>
+							Reset
+						</Button>
+					</div>
+				</Form.Item>
+			</Form>
+		</div>
+	)
 }
 
 export default SubmitPFB
