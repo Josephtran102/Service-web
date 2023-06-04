e4b14534f01b7b0e028401a16a8b7f58cdd9848d
vladsubbotin1 <vladislav.subbotin2010@gmail.com>
2023-06-04 04:13:43 +0400

    delete: explorer field in data

--- data/projects.jsx
+++ data/projects.jsx
@@ -586,7 +586,6 @@ sudo mv $HOME/elys/build/elysd $(which elysd)`,
 			path: '.empowerchain',
 			peerID: 'c413d3d16e250ddbd8f8d495204b2de46ef36b63',
 			seedID: 'c597ec01e412d6e0f62c6f5501224b7fb8393912',
-			explorer: '',
 			peerPort: '16656',
 			seedPort: '16656',
 			installBin: `cd $HOME
