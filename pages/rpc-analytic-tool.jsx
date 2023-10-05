f5aee135ded2bedf9fca3dc014a2b5f2ee0e5791
vladsubbotin1 <vladislav.subbotin2010@gmail.com>
2023-10-05 05:55:48 +0400

    add: public rpc page

---
+++ pages/rpc-analytic-tool.jsx
@@ -0,0 +1,122 @@
+import Head from 'next/head'
+import { Table, Alert } from 'antd'
+import axios from 'axios'
+import Header from '@components/Header'
+import styles from '@styles/Services.module.scss'
+import prettyMs from 'pretty-ms'
+import { WarningFilled } from '@ant-design/icons'
+
+const PublicRPC = ({ data }) => {
+	const dataArray = Object.keys(data).map((key, index) => ({
+		key: index,
+		endpoint: key,
+		blockHeight: data[key].latest_block_height,
+		txIndex: data[key].tx_index,
+		moniker: data[key].moniker,
+		validator: data[key].voting_power,
+		scan_time: data[key].scan_time
+	}))
+
+	const numberOfNetworks = dataArray.length
+	const truncatedDateStr = dataArray.length ? dataArray[0].scan_time.slice(0, 23) + 'Z' : null
+	const lastScanDate = truncatedDateStr ? new Date(truncatedDateStr) : null
+	const now = new Date()
+	const timeDifference = Math.abs(lastScanDate ? now - lastScanDate : 0)
+	const lastScanTimeHumanReadable = timeDifference ? prettyMs(timeDifference, { compact: true }) : 'N/A'
+
+	const columns = [
+		{
+			title: 'Endpoint',
+			dataIndex: 'endpoint',
+			key: 'endpoint',
+			render: text => (
+				<a href={`http://${text}`} target='_blank' rel='noopener noreferrer'>
+					{text}
+				</a>
+			)
+		},
+		{
+			title: 'Block Height',
+			dataIndex: 'blockHeight',
+			key: 'blockHeight'
+		},
+		{
+			title: 'Tx Index',
+			dataIndex: 'txIndex',
+			key: 'txIndex'
+		},
+		{
+			title: 'Moniker',
+			dataIndex: 'moniker',
+			key: 'moniker'
+		},
+		{
+			title: 'Validator',
+			dataIndex: 'validator',
+			key: 'validator',
+			render: validator =>
+				validator !== '0' ? (
+					<>
+						<WarningFilled style={{ color: '#ffcc00' }} /> yes
+					</>
+				) : (
+					'no'
+				)
+		}
+	]
+
+	return (
+		<>
+			<Head>
+				<title>Services - ITRocket</title>
+				<meta name='description' content='ITRocket 🚀 | Crypto Multipurpose Project' />
+			</Head>
+
+			<Header />
+			<div className='w-full p-1 md:p-6 bg-white dark:bg-zinc-900/40'>
+				<div
+					className={styles.mainColumn__wrapper}
+					style={{
+						width: '100%',
+						padding: '10px',
+						marginTop: '60px'
+					}}
+				>
+					<div className={styles.mainColumn}>
+						<h1>Public RPC endpoints ({numberOfNetworks})</h1>
+						<h3>The last scan happened {lastScanTimeHumanReadable} ago.</h3>
+						<Alert
+							message="We don't manage these nodes or validate the accuracy of the data they supply. We recommend using ITRocket managed nodes for more reliable information. Block heights are updated hourly."
+							type='info'
+							showIcon
+							className='!w-fit my-3'
+						/>
+						<Table dataSource={dataArray} columns={columns} pagination={false} bordered />
+						<Alert
+							message='Validators or public sentries which hold voting power above 0 are marked with warning symbol. Exposed to the public network, endpoints can be used as attack vector to harm the chain. Node operators should be aware of this and have a firewall rules in place to limit the attack surface of their validator infrastructure.'
+							type='warning'
+							showIcon
+							className='!w-fit my-3'
+						/>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export async function getServerSideProps() {
+	try {
+		const response = await axios.get('https://testnet-files.itrocket.net/source/.rpc_combined.json')
+		return {
+			props: { data: response.data }
+		}
+	} catch (error) {
+		console.error('An error occurred while fetching data:', error)
+		return {
+			props: { data: {} }
+		}
+	}
+}
+
+export default PublicRPC
