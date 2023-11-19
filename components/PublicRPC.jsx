691c52c52e5f649bed3ee12af7d228e659408392
Vladislav <subbotin.vladd@gmail.com>
2023-11-19 18:29:30 +0400

    add: public RPC component

---
+++ components/PublicRPC.jsx
@@ -0,0 +1,118 @@
+// PublicRPC.js
+import { Table, Alert } from 'antd'
+import { WarningFilled } from '@ant-design/icons'
+import styles from '@styles/Services.module.scss'
+import prettyMilliseconds from 'pretty-ms'
+
+const parseTime = snapTime => {
+	snapTime = Date.parse(snapTime.concat(':00'))
+	snapTime = Date.now() - snapTime
+	const time = prettyMilliseconds(snapTime, { compact: true })
+	return `${time} ago`
+}
+
+const PublicRPC = ({ data, projectName, type }) => {
+	const dynamicLink = `https://${type}-files.itrocket.net/${projectName}/.rpc_combined.json`
+
+	const dataArray = Object.keys(data).map((key, index) => ({
+		key: index,
+		endpoint: key,
+		latest_block_height: data[key].latest_block_height || 'N/A',
+		earliest_block_height: data[key].earliest_block_height || 'N/A',
+		txIndex: data[key].tx_index,
+		moniker: data[key].moniker,
+		validator: data[key].voting_power,
+		scan_time: parseTime(data[key].scan_time)
+	}))
+
+	const columns = [
+		{
+			title: 'Endpoint',
+			dataIndex: 'endpoint',
+			key: 'endpoint',
+			width: 300,
+			render: text => (
+				<a href={`http://${text}`} className='!font-medium' target='_blank' rel='noopener noreferrer'>
+					{text}
+				</a>
+			)
+		},
+		{
+			title: 'Block Height',
+			key: 'blockHeight',
+			align: 'right',
+			render: (_, record) => `${record.earliest_block_height} - ${record.latest_block_height}`
+		},
+		{
+			title: 'Tx Index',
+			dataIndex: 'txIndex',
+			key: 'txIndex',
+			width: 110
+		},
+		{
+			title: 'Moniker',
+			dataIndex: 'moniker',
+			key: 'moniker'
+		},
+		{
+			title: 'Validator',
+			dataIndex: 'validator',
+			key: 'validator',
+			render: validator =>
+				validator !== '0' ? (
+					<>
+						<WarningFilled style={{ color: '#ffcc00' }} /> yes
+					</>
+				) : (
+					'no'
+				),
+			width: 130
+		},
+		{
+			title: 'Scan Time',
+			dataIndex: 'scan_time',
+			key: 'scan_time',
+			width: 140
+		}
+	]
+
+	return (
+		<>
+			<div className={`${styles.mainColumn} bg-[#fff] dark:bg-[#1b1b1b]`}>
+				<h2 id='public-rpc'>Public RPC endpoints: {dataArray.length} active</h2>
+
+				<Alert
+					message="RPC nodes provide real-time chain data. We scan HTTP and HTTPS public RPC nodes every 4h to compile a list of endpoints. We don't manage these nodes or validate their data. Please verify accuracy on your own."
+					type='info'
+					showIcon
+					className='!w-fit mb-4'
+				/>
+				<Table
+					dataSource={dataArray}
+					columns={columns}
+					pagination={false}
+					scroll={{
+						x: 750,
+						y: 340
+					}}
+					bordered
+					size='small'
+					style={{ maxWidth: '1030px' }}
+				/>
+				<p className='!mt-4 !mb-1'>
+					<a href={dynamicLink} target='_blank' rel='noopener noreferrer' style={{ color: '#6a6cff' }}>
+						Raw scan results
+					</a>
+				</p>
+				<Alert
+					message='Validators or public sentries which hold voting power above 0 are marked with warning symbol. Exposed to the public network, endpoints can be used as attack vector to harm the chain. Node operators should be aware of this and have a firewall rules in place to limit the attack surface of their validator infrastructure.'
+					type='warning'
+					showIcon
+					className='!w-fit my-3'
+				/>
+			</div>
+		</>
+	)
+}
+
+export default PublicRPC
--- pages/services/mainnet/[projectName]/public-rpc.jsx
+++ pages/services/mainnet/[projectName]/public-rpc.jsx
@@ -1,83 +1,12 @@
+// [projectName].js
 import Head from 'next/head'
-import { Table, Alert } from 'antd'
 import axios from 'axios'
-import styles from '@styles/Services.module.scss'
-import prettyMilliseconds from 'pretty-ms'
-import { WarningFilled } from '@ant-design/icons'
+
+import PublicRPC from '@components/PublicRPC'
 import { getLayout } from '@layouts/dashboard'
 
-const parseTime = snapTime => {
-	snapTime = Date.parse(snapTime.concat(':00'))
-	snapTime = Date.now() - snapTime
-	const time = prettyMilliseconds(snapTime, { compact: true })
-	return `${time} ago`
-}
-
-const type = 'mainnet'
-
-const PublicRPC = ({ data, projectName }) => {
-	const dynamicLink = `https://${type}-files.itrocket.net/${projectName}/.rpc_combined.json`
-
-	const dataArray = Object.keys(data).map((key, index) => ({
-		key: index,
-		endpoint: key,
-		latest_block_height: data[key].latest_block_height || 'N/A',
-		earliest_block_height: data[key].earliest_block_height || 'N/A',
-		txIndex: data[key].tx_index,
-		moniker: data[key].moniker,
-		validator: data[key].voting_power,
-		scan_time: parseTime(data[key].scan_time)
-	}))
-
-	const columns = [
-		{
-			title: 'Endpoint',
-			dataIndex: 'endpoint',
-			key: 'endpoint',
-			render: text => (
-				<a href={`http://${text}`} className='!font-medium' target='_blank' rel='noopener noreferrer'>
-					{text}
-				</a>
-			)
-		},
-		{
-			title: 'Block Height',
-			key: 'blockHeight',
-			align: 'right',
-			render: (_, record) => `${record.earliest_block_height} - ${record.latest_block_height}`
-		},
-		{
-			title: 'Tx Index',
-			dataIndex: 'txIndex',
-			key: 'txIndex',
-			width: 110
-		},
-		{
-			title: 'Moniker',
-			dataIndex: 'moniker',
-			key: 'moniker'
-		},
-		{
-			title: 'Validator',
-			dataIndex: 'validator',
-			key: 'validator',
-			render: validator =>
-				validator !== '0' ? (
-					<>
-						<WarningFilled style={{ color: '#ffcc00' }} /> yes
-					</>
-				) : (
-					'no'
-				),
-			width: 130
-		},
-		{
-			title: 'Scan Time',
-			dataIndex: 'scan_time',
-			key: 'scan_time',
-			width: 140
-		}
-	]
+const ProjectPage = ({ data, projectName }) => {
+	const type = 'mainnet'
 
 	return (
 		<>
@@ -85,46 +14,14 @@ const PublicRPC = ({ data, projectName }) => {
 				<title>Public RPC - ITRocket</title>
 				<meta name='description' content='ITRocket 🚀 | Crypto Multipurpose Project' />
 			</Head>
-
-			<div className={`${styles.mainColumn} bg-[#fff] dark:bg-[#1b1b1b]`}>
-				<h2 id='public-rpc'>Public RPC endpoints: {dataArray.length} active</h2>
-
-				<Alert
-					message="RPC nodes provide real-time chain data. We scan HTTP and HTTPS public RPC nodes every 4h to compile a list of endpoints. We don't manage these nodes or validate their data. Please verify accuracy on your own."
-					type='info'
-					showIcon
-					className='!w-fit mb-4'
-				/>
-				<Table
-					dataSource={dataArray}
-					columns={columns}
-					pagination={false}
-					scroll={{
-						x: 750,
-						y: 340
-					}}
-					bordered
-					size='small'
-					style={{ maxWidth: '1030px' }}
-				/>
-				<p className='!mt-4 !mb-1'>
-					<a href={dynamicLink} target='_blank' rel='noopener noreferrer' style={{ color: '#6a6cff' }}>
-						Raw scan results
-					</a>
-				</p>
-				<Alert
-					message='Validators or public sentries which hold voting power above 0 are marked with warning symbol. Exposed to the public network, endpoints can be used as attack vector to harm the chain. Node operators should be aware of this and have a firewall rules in place to limit the attack surface of their validator infrastructure.'
-					type='warning'
-					showIcon
-					className='!w-fit my-3'
-				/>
-			</div>
+			<PublicRPC data={data} projectName={projectName} type={type} />
 		</>
 	)
 }
 
 export async function getServerSideProps(context) {
 	const projectName = context.params.projectName
+	const type = 'mainnet'
 	try {
 		const response = await axios.get(`https://${type}-files.itrocket.net/${projectName}/.rpc_combined.json`)
 		return {
@@ -141,5 +38,5 @@ export async function getServerSideProps(context) {
 	}
 }
 
-PublicRPC.getLayout = getLayout
-export default PublicRPC
+ProjectPage.getLayout = getLayout
+export default ProjectPage
