bd6d7bf20146263b82ac4a4acbceb8e9eed9c848
Vladislav <subbotin.vladd@gmail.com>
2023-11-20 20:00:15 +0400

    fix: namada autoinstall

--- pages/api/namada/autoinstall.js
+++ pages/api/namada/autoinstall.js
@@ -172,12 +172,12 @@ echo done
 
 printGreen "7. Configuring custom ports..." && sleep 1
 # Set custom ports in config.toml
-sed -i.bak -e "s%:26658%:${NAMADA_PORT}658%g;
-s%:26657%:${NAMADA_PORT}657%g;
-s%:26656%:${NAMADA_PORT}656%g;
-s%:26545%:${NAMADA_PORT}545%g;
-s%^external_address = ""%external_address = "$(wget -qO- eth0.me):${NAMADA_PORT}656"%;
-s%:26660%:${NAMADA_PORT}660%g" $HOME/.local/share/namada/${CHAIN_ID}/config.toml
+sed -i.bak -e "s%:26658%:\${NAMADA_PORT}658%g;
+s%:26657%:\${NAMADA_PORT}657%g;
+s%:26656%:\${NAMADA_PORT}656%g;
+s%:26545%:\${NAMADA_PORT}545%g;
+s%^external_address = ""%external_address = "$(wget -qO- eth0.me):\${NAMADA_PORT}656"%;
+s%:26660%:\${NAMADA_PORT}660%g" $HOME/.local/share/namada/\${CHAIN_ID}/config.toml
 sleep 1
 echo done
 
