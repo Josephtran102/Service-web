0da605694cecb86bb896acde47049bc8b8713d5a
Vladislav <subbotin.vladd@gmail.com>
2023-12-28 00:00:59 +0400

    fix: api

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,19 +1,28 @@
-import { verifyToken } from '../utils/auth'
+import { verifyToken } from '@utils/auth'
+import prismadb from '@utils/prismadb'
+import cookie from 'cookie'
 
-function isAdmin(req, res, next) {
-	const token = req.headers.authorization
+async function isAdmin(req, res, next) {
+	const cookies = cookie.parse(req.headers.cookie || '')
+	const token = cookies['token']
+
 	if (!token) {
-		return res.status(401).json({ error: 'Unauthorized' })
+		return res.status(401).json({ error: 'Unauthorized: No token provided' })
 	}
+
 	try {
 		const user = verifyToken(token)
-		if (user.role !== 'admin') {
-			return res.status(403).json({ error: 'Forbidden' })
+		const currentUser = await prismadb.user.findUnique({ where: { id: user.id } })
+
+		if (currentUser && currentUser.role === 'admin') {
+			req.user = currentUser
+			next()
+		} else {
+			return res.status(403).json({ error: 'Forbidden: User is not an admin' })
 		}
-		req.user = user
-		next()
 	} catch (err) {
-		return res.status(401).json({ error: 'Invalid token' })
+		console.error('Authentication error', err)
+		return res.status(401).json({ error: 'Invalid token or user not found' })
 	}
 }
 
