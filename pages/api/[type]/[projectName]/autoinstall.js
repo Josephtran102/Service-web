22cb63bc88631c8b67a003eff5653420e6c0352f
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2023-12-30 13:08:00 +0400

    Update autoinstall.js

--- pages/api/[type]/[projectName]/autoinstall.js
+++ pages/api/[type]/[projectName]/autoinstall.js
@@ -114,7 +114,6 @@ sleep 1
 printGreen "1. Installing go..." && sleep 1
 # install go, if needed
 cd $HOME
-! [ -x "$(command -v go)" ] && {
 VER="${goVersion}"
 wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
@@ -123,7 +122,6 @@ rm "go$VER.linux-amd64.tar.gz"
 [ ! -f ~/.bash_profile ] && touch ~/.bash_profile
 echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
 source $HOME/.bash_profile
-}
 [ ! -d ~/go/bin ] && mkdir -p ~/go/bin
 
 echo $(go version) && sleep 1
@@ -175,7 +173,7 @@ s%^external_address = \\"\\"%external_address = \\"$(wget -qO- eth0.me):\${${var
 s%:26660%:\${${variable}_PORT}660%g" $HOME/${path}/config/config.toml
 
 # config pruning
-sed -i -e "s/^pruning *=.*/pruning = \\"nothing\\"/" $HOME/${path}/config/app.toml
+sed -i -e "s/^pruning *=.*/pruning = \\"custom\\"/" $HOME/${path}/config/app.toml
 sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \\"100\\"/" $HOME/${path}/config/app.toml
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \\"50\\"/" $HOME/${path}/config/app.toml
 
