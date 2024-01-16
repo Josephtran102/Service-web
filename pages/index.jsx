a00b712c2e4fff9845a6e208a876a62f86a06f7b
Vladislav <subbotin.vladd@gmail.com>
2024-01-17 03:41:45 +0400

    fix: new hero design

--- pages/index.jsx
+++ pages/index.jsx
@@ -151,8 +151,9 @@ const Home = () => {
 						animate='visible'
 						variants={videoFadeIn}
 						className='absolute w-full h-full object-cover -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 -z-10'
-						src={videoSrc}
-					/>
+					>
+						<source src={videoSrc} type='video/webm' />
+					</motion.video>
 
 					<div className='absolute w-full h-full bg-black opacity-20 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'></div>
 
