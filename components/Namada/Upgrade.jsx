45ad6601cd29ada6d0ad90de11d4940a2f7fcde4
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2024-02-06 12:18:45 +0400

    Update Upgrade.jsx

--- components/Namada/Upgrade.jsx
+++ components/Namada/Upgrade.jsx
@@ -50,10 +50,10 @@ const Upgrade = ({ name, type }) => {
 rm -rf namada
 git clone https://github.com/anoma/namada
 cd namada
-wget https://github.com/anoma/namada/releases/download/v0.31.0/namada-v0.31.0-Linux-x86_64.tar.gz
-tar -xvf namada-v0.31.0-Linux-x86_64.tar.gz
-rm namada-v0.31.0-Linux-x86_64.tar.gz
-cd namada-v0.31.0-Linux-x86_64
+wget https://github.com/anoma/namada/releases/download/v0.31.1/namada-v0.31.1-Linux-x86_64.tar.gz
+tar -xvf namada-v0.31.1-Linux-x86_64.tar.gz
+rm namada-v0.31.1-Linux-x86_64.tar.gz
+cd namada-v0.31.1-Linux-x86_64
 sudo mv namad* /usr/local/bin/
 sudo systemctl restart namadad && sudo journalctl -u namadad -f`}
 				></CodeBlock>
