1a93b17d1160e6c94028945cfb34297584ace9c1
Vladislav <subbotin.vladd@gmail.com>
2024-02-11 22:20:00 +0400

    add: next-themes pckg

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,21 @@
-import React, { useContext } from 'react'
-import { ConfigProvider, theme as AntTheme } from 'antd'
+import { theme as AntTheme, ConfigProvider } from 'antd'
+import { useContext } from 'react'
 
-import { Context, ContextProvider } from 'context/context'
 import '@styles/globals.scss'
+import { Context, ContextProvider } from 'context/context'
+import { ThemeProvider } from 'next-themes'
 
 function App({ Component, pageProps, router }) {
 	const getLayout = Component.getLayout || (page => page)
 
 	return (
 		<ContextProvider>
-			<ThemeProvider>{getLayout(<Component {...pageProps} key={router.route} />)}</ThemeProvider>
+			<CustomThemeProvider>{getLayout(<Component {...pageProps} key={router.route} />)}</CustomThemeProvider>
 		</ContextProvider>
 	)
 }
 
-function ThemeProvider({ children }) {
+function CustomThemeProvider({ children }) {
 	const { theme } = useContext(Context)
 	const { defaultAlgorithm, darkAlgorithm } = AntTheme
 
@@ -34,7 +35,7 @@ function ThemeProvider({ children }) {
 				}
 			}}
 		>
-			{children}
+			<ThemeProvider attribute='class'>{children}</ThemeProvider>
 		</ConfigProvider>
 	)
 }
