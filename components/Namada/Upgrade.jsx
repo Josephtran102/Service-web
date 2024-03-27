173e5822ebdc1073b6b79178c2ba13f9ee7006d1
Marutyan Samvel <48280694+itrocket-team@users.noreply.github.com>
2024-03-27 11:36:00 +0400

    Update Upgrade.jsx

--- components/Namada/Upgrade.jsx
+++ components/Namada/Upgrade.jsx
@@ -45,15 +45,15 @@ const Upgrade = ({ name, type }) => {
 				style={{ backgroundColor: theme === 'light' ? '#fff' : '#1b1b1b' }}
 			>
 				<CodeBlock
-					desc='Upgrade to v0.31.9'
+					desc='Upgrade to v0.31.10'
 					code={`cd $HOME
 rm -rf namada
 git clone https://github.com/anoma/namada
 cd namada
-wget https://github.com/anoma/namada/releases/download/v0.31.9/namada-v0.31.9-Linux-x86_64.tar.gz
-tar -xvf namada-v0.31.9-Linux-x86_64.tar.gz
-rm namada-v0.31.9-Linux-x86_64.tar.gz
-cd namada-v0.31.9-Linux-x86_64
+wget https://github.com/anoma/namada/releases/download/v0.31.10/namada-v0.31.10-Linux-x86_64.tar.gz
+tar -xvf namada-v0.31.10-Linux-x86_64.tar.gz
+rm namada-v0.31.10-Linux-x86_64.tar.gz
+cd namada-v0.31.10-Linux-x86_64
 sudo mv namad* /usr/local/bin/
 sudo systemctl restart namadad && sudo journalctl -u namadad -f`}
 				></CodeBlock>
